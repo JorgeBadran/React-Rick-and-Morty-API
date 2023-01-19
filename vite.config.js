a1ed041a51cbf22eda999a6c2e0533e60c0148c8
Jorge Badran <jorgebadran08@gmail.com>
2023-01-19 12:23:34 -0500

    Se actualizo el archivo vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://jorgebadran.github.io/React-Rick-and-Morty-API/",
-  plugins: [react()],
+  base: "https://JorgeBadran.github.io/React-Rick-and-Morty-API/",
+  plugins: [react()]
 })
